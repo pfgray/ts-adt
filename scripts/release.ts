36200f69c38fc2ed8524f0425f68176cbf095386
Paul Gray <pfbgray@gmail.com>
2020-12-06 21:55:55 -0500

    Adding release script

---
+++ scripts/release.ts
@@ -0,0 +1,26 @@
+import { run } from "./run";
+import * as child_process from "child_process";
+import { left, right } from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
+
+const DIST = "lib";
+
+const exec = (
+  cmd: string,
+  args?: child_process.ExecOptions
+): TE.TaskEither<Error, void> => () =>
+  new Promise((resolve) => {
+    child_process.exec(cmd, args, (err) => {
+      if (err !== null) {
+        return resolve(left(err));
+      }
+
+      return resolve(right(undefined));
+    });
+  });
+
+export const main = exec("npm publish", {
+  cwd: DIST,
+});
+
+run(main);
